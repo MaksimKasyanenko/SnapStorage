d1bbcbb6894ac54d4af1c8cccc029e7a385c1494
Максим Касьяненко <maksim93012@gmail.com>
2024-05-05 01:37:30 +0300

    go to vue

---
+++ SnapStorageVueApp/src/components/TopPanel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="shadow">
+        <header class="header main-header">
+            <div style="padding: .2rem;">
+                <img src="../icons/logo-32.png" style="max-width: 16px">
+            </div>
+
+            <div class="command-panel ms-auto">
+                <button title="Create a new preset" class="ms-auto me-2"><strong>New
+                        preset</strong></button>
+                <button title="Clear localstorage">Clear LocalStorage</button>
+                <button title="Clear session storage">Clear SessionStorage</button>
+                <button title="Clear Cookie">Clear Cookie</button>
+            </div>
+        </header>
+    </div>
+</template>
+
+<style scoped></style>
+
+<script setup></script>
