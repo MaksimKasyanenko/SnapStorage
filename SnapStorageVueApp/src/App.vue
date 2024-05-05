4e9c2de6dd3f15a7a865d4fa7e954029deb94450
Максим Касьяненко <maksim93012@gmail.com>
2024-05-05 20:50:06 +0300

    add Scroller component

--- SnapStorageVueApp/src/App.vue
+++ SnapStorageVueApp/src/App.vue
@@ -2,13 +2,17 @@
 import TopPanel from './components/TopPanel.vue';
 import Notification from './components/Notification.vue';
 import EventEmitter from './components/EventEmitter.vue';
+import Scroller from './components/Scroller.vue';
 
 </script>
 
 <template>
   <EventEmitter />
-  
+
   <TopPanel />
+  <Scroller>
+    <ul class="preset-list" id="presetList"></ul>
+  </Scroller>
 
   <Notification />
 </template>
---
+++ SnapStorageVueApp/src/components/Scroller.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="scroller">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.scroller {
+    height: 400px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+}
+
+.scroller::-webkit-scrollbar {
+    display: none;
+}
+</style>
