2fd74583a0c0bf4b56ddd2c3c138eb07ad95b5a8
Максим Касьяненко <maksim93012@gmail.com>
2024-05-11 23:33:19 +0300

    fixes in preset list

--- SnapStorageVueApp/src/components/PresetListItem.vue
+++ SnapStorageVueApp/src/components/PresetListItem.vue
@@ -1,27 +1,16 @@
-<template>
-<li>
-    <div v-on:click="onPresetClick">{{ props.preset.name }}</div>
-    <div class="btn-container">
-        <button class="p-0 me-1" v-on:click="onEditButtonClick">
-            <img class="icon" src="./icons/edit-ui-svgrepo-com.vue">
-        </button>
-        <button class="p-0" v-on:click="onDeleteButtonClick">
-            <img class="icon" src="./icons/delete-svgrepo-com.vue">
-        </button>
-    </div>
-</li>
-</template>
-
 <script setup>
+import EditIcon from './icons/EditIcon.vue';
+import DeleteIcon from './icons/DeleteIcon.vue';
+
 const props = defineProps(['preset']);
 
 async function onPresetClick() {
-    if (preset.clearStorage) {
+    if (props.preset.clearStorage) {
         //sendToContentScript("clear-storage", { storageType: presets[i].storageType });
     }
                 
     //sendToContentScript("set-to-storage", { items: presets[i].items, storageType: presets[i].storageType });
-    window.globalEventEmitter.emit('notify', `"${preset.name}" applied`);
+    window.globalEventEmitter.emit('notify', `"${props.preset.name}" applied`);
 }
 
 function onEditButtonClick() {
@@ -37,6 +26,20 @@ async function onDeleteButtonClick() {
     //drawPresetList();
 }
 </script>
+
+<template>
+<li>
+    <div class="preset-name-text" v-on:click="onPresetClick">{{ props.preset.name }}</div>
+    <div class="btn-container">
+        <button class="p-0 me-1" v-on:click="onEditButtonClick">
+            <EditIcon />
+        </button>
+        <button class="p-0" v-on:click="onDeleteButtonClick">
+            <DeleteIcon />
+        </button>
+    </div>
+</li>
+</template>
 
 <style scoped>
 li {
